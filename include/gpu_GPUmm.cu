8b3934a6d84c03a1b35b62cc1fd4ae57847ae3aa
sky12hi12 <kouki.maeda@keio.jp>
2025-06-12 00:56:46 +0900

    Update gpu_GPUmm.cu

--- include/gpu_GPUmm.cu
+++ include/gpu_GPUmm.cu
@@ -306,8 +306,8 @@ sparseSparseMM(cusparseHandle_t handle, cusparseMatDescr_t descr_old,
                                  (void**)&csr_colind_C,
                                  (void**)&csr_val_C, NULL, NULL, NULL, NULL));
 
-    CUDA_CALL(cudaMalloc(&csr_colind_C, sizeof(int)   * nnz_C));
-    CUDA_CALL(cudaMalloc(&csr_val_C,    sizeof(float) * nnz_C));
+    //CUDA_CALL(cudaMalloc(&csr_colind_C, sizeof(int)   * nnz_C));
+    //CUDA_CALL(cudaMalloc(&csr_val_C,    sizeof(float) * nnz_C));
     CUSPARSE_CALL(cusparseCsrSetPointers(matC, csr_rowptr_C, csr_colind_C, csr_val_C));
     
     // SpGEMM Phase 2: Compute
